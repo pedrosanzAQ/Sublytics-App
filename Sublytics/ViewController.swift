b72c99c26d70fbf42df40533c549ca817b40cf45
pedrosanz <pedroalfonsosanz@gmail.com>
2026-03-04 15:06:37 -0600

    Initial Commit

---
+++ Sublytics/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sublytics
+//
+//  Created by pedrosanz on 04/03/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SublyticsTests/SublyticsTests.swift
@@ -0,0 +1,17 @@
+//
+//  SublyticsTests.swift
+//  SublyticsTests
+//
+//  Created by pedrosanz on 04/03/26.
+//
+
+import Testing
+@testable import Sublytics
+
+struct SublyticsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
